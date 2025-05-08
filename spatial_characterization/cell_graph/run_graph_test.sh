bfb1ab57e3b669d291f11647fe1fdba7b709ced9
Xiang <xl260@duke.edu>
2025-05-08 01:43:09 +0000

    update env

---
+++ spatial_characterization/cell_graph/run_graph_test.sh
@@ -0,0 +1,7 @@
+python run_graph.py \
+--input_dir_img="../cell_segmentation/test_data/input" \
+--input_dir_mat="../cell_segmentation/test_data/output/mat" \
+--output_dir="./test_graph_output" \
+--marker_size=0.1 \
+--line_width=0.3 \
+--cell_dist=40 \
